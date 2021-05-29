50832ce2105fb7421fa7c7fff3015cf4c4756c5e
Ugo Varetto <ugovaretto@gmail.com>
2021-05-29 22:50:34 +0800

    ...

---
+++ ghutton/connect4.hs
@@ -0,0 +1,235 @@
+module Main where
+
+import Data.List
+import SortedTree
+
+-- Entry point
+main :: IO ()
+main = play X initial
+
+-- Data types & global config
+-- Transposed board to make is easier to append to rows instead of columns
+rows :: Int
+rows = 7
+
+cols :: Int
+cols = 6
+
+depth :: Int
+depth = 4
+
+win :: Int
+win = 4
+
+initial :: Board
+initial = replicate rows []
+
+data Player = O | B | X deriving (Show, Ord, Eq)
+
+type Matrix = [Row]
+
+type Board = [Row]
+
+type Row = [Player]
+
+type Column = [Player]
+
+-- Play game
+play :: Player -> Board -> IO ()
+play player board = do
+  putStrLn $ showBoard board --print filled board
+  let t = tag board -- check tag, if X or O somebody is the winner
+  if t `elem` [X, O]
+    then print ("Player " ++ show t ++ " WINS!")
+    else
+      if computer player
+        then do
+          print "Thinking..."
+          let move = bestMove player board
+          print ("column " ++ show move)
+          let newBoard =
+                append player move board
+          play (nextPlayer player) newBoard
+        else do
+          print "Select a row between 1 and 7"
+          x <- getLine
+          let c = (read x :: Int) - 1
+          if c > 6 || c < 0
+            then play player board
+            else
+              play
+                (nextPlayer player)
+                (append player c board)
+
+computer :: Player -> Bool
+computer p
+  | p == O = True
+  | otherwise = False
+
+nextPlayer :: Player -> Player
+nextPlayer X = O
+nextPlayer O = X
+
+append :: Player -> Int -> Board -> Board
+append p row b =
+  let (lss, rs : rss) = splitAt row b
+   in lss ++ [rs ++ [p]] ++ rss
+
+maxPlayer :: Player -> Bool
+maxPlayer X = True
+maxPlayer O = False
+
+-- I/O
+showBoard :: Board -> String
+showBoard [] = ""
+showBoard b =
+  let bi = fillm b
+   in concat
+        [ if null xs
+            then ""
+            else map toSym xs ++ "\n"
+          | xs <- bi
+        ]
+
+fillm :: Matrix -> Matrix
+fillm [] = []
+fillm m =
+  [ xs
+      ++ ( if cols - length xs > 0
+             then replicate (cols - length xs) B
+             else []
+         )
+    | xs <- m
+  ]
+
+toSym :: Player -> Char
+toSym p
+  | p == O = 'o'
+  | p == X = 'x'
+  | otherwise = '.'
+
+-- Look for winning sequences: diagonals, rows, columns
+tag :: Matrix -> Player
+tag [] = B
+tag m =
+  let ws = filter (not . null) $ concatMap (winners win) $ allCells (fillm m)
+   in if null ws || null (head ws) then B else head $ head ws
+
+winners :: Eq a => Int -> [a] -> [[a]]
+winners _ [] = []
+winners w ws = filter (\xs -> length xs == w) (allSeq ws)
+
+allCells :: Matrix -> [Row]
+allCells [] = []
+allCells m = allDiags m ++ m ++ trans m
+
+allSeq :: Eq a => [a] -> [[a]]
+allSeq [] = []
+allSeq [x] = [[x]]
+allSeq (x : xs) =
+  let (l, r) = span (== x) (x : xs)
+   in l : allSeq r
+
+allDiags :: Matrix -> [Row]
+allDiags [] = []
+allDiags m =
+  map leftDiag (appSeq id m)
+    ++ map rightDiag (appSeq id m)
+    ++ map leftDiag (m : appSeq tailCols m)
+    ++ map rightDiag (m : appSeq tailCols m)
+
+trans :: Matrix -> Matrix
+trans [] = []
+trans [[x]] = [[x]]
+trans ([] : _) = [] -- [[],[],[]...] case
+trans m = map head m : trans (map tail m)
+
+leftDiag :: Matrix -> Row
+leftDiag [] = []
+leftDiag [[]] = []
+leftDiag [[x]] = [x]
+leftDiag m = head (head m) : leftDiag (tailRows (tailCols m))
+
+rightDiag :: Matrix -> Row
+rightDiag [] = []
+rightDiag [[]] = []
+rightDiag [[x]] = [x]
+rightDiag m = last (head m) : rightDiag (tailRows (initCols m))
+
+tailRows :: Matrix -> Matrix
+tailRows [] = []
+tailRows [[]] = []
+tailRows [[_]] = []
+tailRows m = tail m
+
+tailCols :: Matrix -> Matrix
+tailCols [] = []
+tailCols [[]] = []
+tailCols [[_]] = []
+tailCols m = (trans . tail . trans) m
+
+initCols :: Matrix -> Matrix
+initCols = map init
+
+appSeq :: ([a] -> [a]) -> [a] -> [[a]]
+appSeq _ [] = []
+appSeq f [x] = [f [x]]
+appSeq f (x : xs) = f (x : xs) : appSeq f xs
+
+full :: Matrix -> Bool
+full [] = False
+full m = all (\xs -> not (null xs) && all (\x -> x `elem` [O,X]) xs) m
+
+-- AI
+bestMove :: Player -> Board -> Int
+bestMove p b =
+  let moves =
+        [ (search p depth $ append p m b, m)
+          | m <- [0 .. 6]
+        ]
+   in selectMove p moves
+
+search :: Player -> Int -> Matrix -> Player
+search _ _ [] = B
+search p d m
+  | tag m `elem` [O, X] = tag m
+  | d == 0 = tag m
+  | full m = B
+  | otherwise =
+    let t =
+          --treeSort
+          --  (<)
+            [ search (nextPlayer p) (d -1) xs
+              | xs <- choices p m
+            ]
+     in 
+       if null t then B
+       else if maxPlayer p then foldr max O t else foldr min X t
+
+selectMove :: Player -> [(Player, Int)] -> Int
+selectMove p xs
+  | maxPlayer p =
+    let (_, x) =
+          foldr
+            ( \(x1, y1) (x2, y2) ->
+                if x1 > x2
+                  then (x1, y1)
+                  else (x2, y2)
+            )
+            (O, 0)
+            xs
+     in x
+  | otherwise =
+    let (_, x) =
+          foldr
+            ( \(x1, y1) (x2, y2) ->
+                if x1 < x2
+                  then (x1, y1)
+                  else (x2, y2)
+            )
+            (X, 0)
+            xs
+     in x
+
+choices :: Player -> Matrix -> [Matrix]
+choices p m = [append p i m | i <- [0..6] ]
--- ghutton/connect_four.hs
+++ ghutton/connect_four.hs
@@ -1,49 +1,59 @@
-{-# OPTIONS_GHC -Wno-missing-methods #-}
 {-# OPTIONS_GHC -Wno-deferred-type-errors #-}
+{-# OPTIONS_GHC -Wno-missing-methods #-}
+
 module Main where
+
 import Data.List
 import SortedTree
 
 rows :: Int
 rows = 6
+
 cols :: Int
 cols = 7
+
 depth :: Int
 depth = 4
+
 win :: Int
 win = 4
 
 data Player = O | B | X deriving (Show, Ord, Eq)
 
 type Matrix = [Row]
+
 type Board = [Row]
+
 type Row = [Player]
+
 type Column = [Player]
 
 emptyBoard :: Board
-emptyBoard = replicate rows $ replicate cols B
+emptyBoard = replicate cols $ replicate rows B
 
 initial :: Board
-initial = replicate rows []
+initial = replicate cols []
 
 trans :: Matrix -> Matrix
 trans [] = []
 trans [[x]] = [[x]]
-trans ([]:_) = [] -- [[],[],[]...] case
+trans ([] : _) = [] -- [[],[],[]...] case
 trans m = map head m : trans (map tail m)
 
-
-allSeq :: Eq a => [a] -> [[a]]
-allSeq [] = []
-allSeq [x] = [[x]]
-allSeq (x:xs) = let (l,r) = span (==x) (x:xs)
-                in l : allSeq r
+allCells :: Matrix -> [Row]
+allCells [] = []
+allCells m = allDiags m ++ m ++ trans m
 
 fillMat :: Int -> Matrix -> Matrix
 fillMat _ [] = []
-fillMat n m = [xs ++ (if n - length xs > 0
-                      then replicate (n - length xs) B
-                     else []) | xs <- m]
+fillMat n m =
+  [ xs
+      ++ ( if n - length xs > 0
+             then replicate (n - length xs) B
+             else []
+         )
+    | xs <- m
+  ]
 
 tailCols :: Matrix -> Matrix
 tailCols = trans . tail . trans
@@ -77,18 +87,24 @@ winners :: Eq a => Int -> [a] -> [[a]]
 winners _ [] = []
 winners w ws = filter (\xs -> length xs == w) (allSeq ws)
 
-appSeq :: ([a]-> [a]) -> [a] -> [[a]]
+appSeq :: ([a] -> [a]) -> [a] -> [[a]]
 appSeq _ [] = []
 appSeq f [x] = [f [x]]
-appSeq f (x:xs) = f (x:xs) : appSeq f xs
-
+appSeq f (x : xs) = f (x : xs) : appSeq f xs
 
 allDiags :: Matrix -> [Row]
 allDiags [] = []
-allDiags m = map leftDiag (appSeq id m) ++
-             map rightDiag (appSeq id m) ++
-             map leftDiag (m : appSeq tailCols m) ++
-             map rightDiag (m : appSeq tailCols m)
+allDiags m =
+  map leftDiag (appSeq id m)
+    ++ map rightDiag (appSeq id m)
+    ++ map leftDiag (m : appSeq tailCols m)
+    ++ map rightDiag (m : appSeq tailCols m)
+
+allSeq :: Eq a => [a] -> [[a]]
+allSeq [] = []
+allSeq [x] = [[x]]
+allSeq (x:xs) = let (l,r) = span (==x) (x:xs)
+                in l : allSeq r
 
 allCells :: Matrix -> [Row]
 allCells [] = []
@@ -96,44 +112,62 @@ allCells m = allDiags m ++ m ++ trans m
 
 tag :: Matrix -> Player
 tag [] = B
-tag m = let ws = filter (not . null) $ concatMap (winners win) $ allCells m
-        in if null ws then B else head $ head ws
+tag m =
+  let ws = filter (not . null) $ concatMap (winners win) $ allCells m
+   in if null ws then B else head $ head ws
 
 append :: Player -> Int -> Board -> Board
-append p row b = let (lss,rs:rss) = splitAt row b
-                 in lss ++ [rs ++ [p]] ++ rss
+append p row b =
+  let (lss, rs : rss) = splitAt row b
+   in lss ++ [rs ++ [p]] ++ rss
 
 boardAppend :: Player -> Int -> Board -> Board
-boardAppend p row = append p (rows - row - 1)
+boardAppend p col = append p (cols - col - 1)
 
 toSym :: Player -> Char
-toSym p | p == O = 'o'
-        | p == X = 'x'
-        | otherwise = '.'
+toSym p
+  | p == O = 'o'
+  | p == X = 'x'
+  | otherwise = '.'
 
 fillm :: Matrix -> Matrix
 fillm [] = []
-fillm m = fillMat rows m
+fillm m = fillMat cols m
 
 showBoard :: Board -> String
 showBoard [] = ""
-showBoard b = let bi = trans (fillMat rows b)
-              in concat [if null xs then ""
-                         else map toSym xs ++ "\n"| xs <- bi]
+showBoard b =
+  let bi = fillMat cols b
+   in concat
+        [ if null xs
+            then ""
+            else map toSym xs ++ "\n"
+          | xs <- bi
+        ]
 
-choices :: Matrix -> [Matrix]
-choices m = choices' [O,X] ([],m) where
-  choices' _ (_,[]) = []
-  choices' p (lss,rs:rss) = [lss ++ [rs ++ [x]] ++ rss
-                             | length rs < rows, x <- p]
-                             ++ choices' p (lss ++ [rs], rss)
-search :: Player -> Int -> Matrix -> (Matrix, Player)
-search _ _ [] = ([],B)
-search p d m | tag m `elem` [O,X] = (m, tag m)
-             | d == 0 = (m, tag m)
-             | otherwise = let t = treeSort (\(_,x) (_,y) -> x < y)
-                                   [search (nextPlayer p) (d-1) x | x <- choices m]
-                           in if maxPlayer p then last t else head t
+choices :: Player -> Matrix -> [Matrix]
+choices p m = choices' ([], m)
+  where
+    choices' (_, []) = []
+    choices' (lss, rs : rss) =
+      [ lss ++ [rs ++ [p]] ++ rss
+        | length rs < cols
+      ]
+        ++ choices' (lss ++ [rs], rss)
+
+search :: Player -> Int -> Matrix -> Player
+search _ _ [] = B
+search p d m
+  | tag m `elem` [O, X] = tag m
+  | d == 0 = tag m
+  | otherwise =
+    let t =
+          treeSort
+            (<)
+            [ search (nextPlayer p) (d -1) x
+              | x <- choices p m
+            ]
+     in if maxPlayer p then last t else head t
 
 maxPlayer :: Player -> Bool
 maxPlayer X = True
@@ -143,47 +177,69 @@ nextPlayer :: Player -> Player
 nextPlayer X = O
 nextPlayer O = X
 
+selectMove :: Player -> [(Player, Int)] -> Int
+selectMove p xs
+  | maxPlayer p =
+    let (_, x) =
+          foldr
+            ( \(x1, y1) (x2, y2) ->
+                if x1 > x2
+                  then (x1, y1)
+                  else (x2, y2)
+            )
+            (O, 0)
+            xs
+     in x
+  | otherwise =
+    let (_, x) =
+          foldr
+            ( \(x1, y1) (x2, y2) ->
+                if x1 < x2
+                  then (x1, y1)
+                  else (x2, y2)
+            )
+            (X, 0)
+            xs
+     in x
+
+bestMove :: Player -> Board -> Int
+bestMove p b =
+  let moves =
+        [ (search p depth $ fillm $ boardAppend p m b, m)
+          | m <- [0 .. 6]
+        ]
+   in selectMove p moves
+
 -- game
--- 0) loop player board:
---   print board
---   check if someone has won
---     True: print winner
---           exit
---     False: continue
---   print current player
---   1) if current player human: 
---        prompt for column
---        if wrong column go to (1)
---        board <- move board
---      else
---        chose move
---        execute move
---        go to (0) nextPlayer player board 
 
 computer :: Player -> Bool
-computer p | p == O = True
-           | otherwise = False
+computer p
+  | p == O = True
+  | otherwise = False
 
 play :: Player -> Board -> IO ()
 play player board = do
-                      putStrLn $ showBoard (fillm board)
-                      let t = tag board
-                      if t `elem` [X,O] then
-                        print ("Player " ++ show t ++ " WINS!")
-                      else
-                        if computer player then do
-                          print "Thinking..."
-                          let (b,_) = search player depth board
-                          play (nextPlayer player) b
-                        else do
-                          print "Select a column between 1 and 7"
-                          x  <- readLn
-                          let c = read x :: Int
-                          if c > 7 || c < 1 then play player board
-                          else play (nextPlayer player) (boardAppend player c board)
-
-
-
+  putStrLn $ showBoard (fillm board)
+  let t = tag (fillm board)
+  if t `elem` [X, O]
+    then print ("Player " ++ show t ++ " WINS!")
+    else
+      if computer player
+        then do
+          print "Thinking..."
+          let newBoard =
+                boardAppend player (bestMove player board) board
+          play (nextPlayer player) newBoard
+        else do
+          print "Select a column between 1 and 7"
+          x <- getLine
+          let c = (read x :: Int) - 1
+          if c > 7 || c < 1
+            then play player board
+            else
+              play
+                (nextPlayer player)
+                (boardAppend player c board)
 
 main :: IO ()
 main = play X initial
