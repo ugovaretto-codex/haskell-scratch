a055e3a34fb087950f70b028fdbd498b5619ef02
ugo <ugovaretto@gmail.com>
2021-07-05 20:09:54 +0800

    ...

---
+++ ghutton/monads1.hs
@@ -0,0 +1,43 @@
+data Expr = Val Int | Div Expr Expr
+
+-- eval :: Expr -> Maybe Int
+-- eval (Val n) = Just n
+-- eval (Div x y) = case eval x of
+--                     Nothing -> Nothing
+--                     Just n -> case eval y of
+--                         Nothing -> Nothing 
+--                         Just m -> safediv n m
+
+-- eval :: Expr -> Maybe Int
+-- eval (Val n) = return n
+-- eval (Div x y) =  eval x >>= (\n -> 
+--                     eval y >>= (\m ->
+--                         safediv n m))
+eval :: Expr -> Maybe Int
+eval (Val n) = return n
+eval (Div x y) = do
+                    n <- eval x
+                    m <- eval y
+                    safediv n m
+
+safediv :: Int -> Int -> Maybe Int
+safediv _ 0 = Nothing
+safediv n m = Just (n `div` m)
+
+type State = 
+
+type ST a = State -> (a, State)
+
+newtype ST a = S (State -> (a,State))
+app :: ST a -> State -> (a, State)
+app (S st) s = st s
+
+-- make it into a functor (fmap) and applicative (pure, <*>)
+
+instance Monad ST where
+    -- return :: a -> ST a
+    return x = S (\s -> (x,s))
+    -- >>= :: ST a -> (a -> ST b) -> ST b
+
+S (\s -> let (x,s') = app st s
+         in app (f x) s'
